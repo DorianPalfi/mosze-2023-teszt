1bfbc4c4fbfefa16b36c3cc96bd0964799f1bde3
Dorian Palfi <dorianpalfi@gmail.com>
2024-09-30 12:24:08 +0200

    comments

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //Alul vonas hianyzik
+    std::cout << '1-100 ertekek duplazasa' //sztring de karakter es nincs lezarva
+    for (int i = 0;) //ciklus nem jol definialt
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //(i+1)*2
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //ertelmetlen"
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //nincs lezarva nem ki erteket
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag; // nincs kezdo ertek
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //nincs lezarva
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
